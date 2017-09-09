423f0d2a4aaf3ad921e3f81c8e650e5d6d8fcb97
mossd3128 <31806211+mossd3128@users.noreply.github.com>
2017-09-09 17:46:02 -0400

    Add files via upload
    2nd Submission

---
+++ M2HW1_DistanceTraveled_DonaldMoss.py
@@ -0,0 +1,19 @@
+# CTI-110
+# M2HW1-Distance Traveled
+# Donald Moss
+# 09/09/2017
+print('A car is traveling at 70 miles per hour.')
+
+# distance = speed * time.
+distanceafter6 = 70 * 6
+distanceafterT10 = 70 * 10
+distanceafter15 = 70 * 15
+
+# Display the distanceafter6.
+print('The distance traveled after 6 hours in miles is', distanceafter6)
+
+# Display the distanceafterT10.
+print('The distance traveled after 10 hours in miles is', distanceafterT10)
+
+# Display the distanceafter15.
+print('The distance traveld after 15 hours in miles is', distanceafter15)
