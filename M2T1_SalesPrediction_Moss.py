bb1797d103534ae16f52d57cdeb1e2b469bc1950
mossd3128 <31806211+mossd3128@users.noreply.github.com>
2017-09-09 16:16:38 -0400

    Add files via upload
    1st Submission

---
+++ M2T1_SalesPrediction_Moss.py
@@ -0,0 +1,12 @@
+# CTI-110
+# M2T1-Sales Prediction
+# Donald Moss
+# 09/09/2017
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, '.2f'))
