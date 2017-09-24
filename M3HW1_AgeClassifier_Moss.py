d958d69532da8404c51c0d459e25d29eb8318de7
mossd3128 <31806211+mossd3128@users.noreply.github.com>
2017-09-24 18:14:47 -0400

    Add files via upload

---
+++ M3HW1_AgeClassifier_Moss.py
@@ -0,0 +1,39 @@
+# CTI-110
+# M3HW1 - Age Classifier
+# Donald Moss
+# 09/24/2017
+
+def main():
+    # This program takes a person's age and determines if the person is an infant, child, teenager, or adult.
+
+    # system uses age requirements for determining results.
+    infant = 1
+    child = 13
+    teenager = 19
+    adult = 20
+    
+
+    
+    age = int(input('Enter age: '))
+
+    if age >= adult:
+        print('Your age is: adult')
+    else:
+        if age < adult:
+            print('Your age is: teenager')
+        else:
+            if age < teenager:
+                print('Your age is: child') 
+            else:
+                if age <= infant:    
+                    print('Your age is: infant')
+                
+
+
+
+
+
+
+
+# program start
+main()
