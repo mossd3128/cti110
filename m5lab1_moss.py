feac1583ddc17a5cb96f03e52ef58a16cb4d1a69
mossd3128 <31806211+mossd3128@users.noreply.github.com>
2017-10-22 18:56:53 -0400

    Add files via upload

---
+++ m5lab1_moss.py
@@ -0,0 +1,23 @@
+# CTI-110
+# M5Lab1
+# Donald Moss
+# 10/22/2017
+
+import turtle
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.penup()
+turtle.forward(100)
+turtle.right(60)
+turtle.pendown()
+turtle.forward(90)
+turtle.right(120)
+turtle.forward(90)
+turtle.right(120)
+turtle.forward(90)             
