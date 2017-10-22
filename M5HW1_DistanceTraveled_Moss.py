b5e81c9e90f56082a9466a0b5668d5cb7039a8b1
mossd3128 <31806211+mossd3128@users.noreply.github.com>
2017-10-22 19:59:34 -0400

    Add files via upload

---
+++ M5HW1_DistanceTraveled_Moss.py
@@ -0,0 +1,16 @@
+# CTI-110
+# M5Lab2
+# Donald Moss
+# 10/22/2017
+
+# Get speed and time
+speed = int(input('Enter the speed of the vehicle in miles per hour:'))
+time = int(input('Enter the number of hours it has traveled:'))
+
+print('Hour \t\t Distance Traveled')
+
+# Calculate distance 
+for time in range(1, time + 1):
+    distance = speed * time
+    print(time, '\t\t\t', distance)
+
